17b7fc61bca0ebf73a3a59f5db9ed7ed0b5a1fa8
ilis137 <32433239+ilis137@users.noreply.github.com>
2018-11-05 19:10:27 +0530

    single input and output added

--- main.js
+++ main.js
@@ -9,6 +9,32 @@ var n = 0;
 $(document).ready(function() {
     $('.outer').click(power)
     $('#start').click(startGame)
+    $("#Yellow").click(function() {
+        console.log("here in");
+        yellow();
+        user.push("Yellow");
+        userPlay();
+    })
+
+    $("#Green").click(function() {
+        console.log("here in");
+        green()
+        user.push("Green")
+        userPlay();
+    })
+
+    $("#Blue").click(function() {
+        console.log("here in");
+        blue()
+        user.push("Blue")
+        userPlay();
+    })
+    $("#Red").click(function() {
+        console.log("here in");
+        green()
+        user.push("Red")
+        userPlay();
+    })
 });
 
 let colors = ["Green", "Red", "Yellow", "Blue"]
@@ -20,12 +46,21 @@ function power() {
         $('.outer').removeClass('outer-inactive');
         $('.outer').addClass('outer-active');
 
+
     } else {
 
 
         $('.outer').removeClass('outer-active');
         $('#start').removeClass('game-active');
         $('.outer').addClass('outer-inactive');
+        $("#Yellow").removeClass("winner");
+        $("#Green").removeClass("winner");
+        $("#Red").removeClass("winner");
+        $("#Blue").removeClass("winner");
+        $("#Yellow").removeClass("loser");
+        $("#Green").removeClass("loser");
+        $("#Red").removeClass("loser");
+        $("#Blue").removeClass("loser");
     }
 }
 
@@ -38,11 +73,33 @@ function startGame() {
     userCount = 0;
     if ($('.outer').hasClass('outer-active')) {
         $('#start').addClass('game-active');
-        runGame();
+        $("#Yellow").removeClass("winner");
+        $("#Green").removeClass("winner");
+        $("#Red").removeClass("winner");
+        $("#Blue").removeClass("winner");
+        $("#Yellow").removeClass("loser");
+        $("#Green").removeClass("loser");
+        $("#Red").removeClass("loser");
+        $("#Blue").removeClass("loser");
+        Machine();
     } else {
         console.log("game is off")
     }
 
+}
+
+function Machine() {
+    var Machine = setTimeout(function() {
+
+        reset()
+        random()
+
+        simon()
+            // count++;
+        setTimeout(function() {
+            reset()
+        }, 500)
+    }, 750)
 }
 
 function reset() {
@@ -90,16 +147,95 @@ function simon() {
         blue()
 }
 
-function runGame() {
-    var Machine = setInterval(function() {
-
-        reset()
-        random()
-        console.log(rand)
-        simon()
-
-        count++;
-    }, 750)
 
 
+
+
+function userPlay() {
+    if (user[userCount] == game[counter] && game.length == user.length && game.length == 5) {
+        {
+            $("#Yellow").addClass("winner");
+            $("#Green").addClass("winner");
+            $("#Red").addClass("winner");
+            $("#Blue").addClass("winner");
+            console.log("game over ,you win!");
+
+        }
+    } else if (user[userCount] == game[counter] && game.length == user.length) {
+        {
+            user = [];
+            userCount = 0;
+            setTimeout(function() {
+                counter++;
+                reset();
+                newRound();
+            }, 500)
+        }
+    } else if (user[userCount] == game[counter]) {
+        console.log("correct!")
+        userCount++;
+        setTimeout(reset(), 500);
+    } else {
+
+        setTimeout(function() {
+            repeatColors()
+        }, 750)
+
+    }
+
+
+}
+
+function repeat() {
+    if (game[n] == "Green") {
+        n++
+        green()
+    } else if (game[n] == "Red") {
+        n++
+        red();
+    } else if (game[n] == "Yellow") {
+        n++;
+        yellow();
+
+    } else {
+        n++;
+        blue();
+    }
+}
+
+function repeatColors() {
+    function clear() {
+        clearInterval(timeFunction);
+    }
+    let timeFunction = setInterval(
+        function() {
+            reset()
+            if (n != game.length) {
+                repeat()
+            } else if (n == game.length) {
+                user = [];
+                userCount = 0;
+                n = 0;
+                clear();
+            }
+        }, 750)
+}
+
+function newRound() {
+    function clear() {
+        clearInterval(timeFunction);
+    }
+    let timeFunction = setInterval(
+        function() {
+            reset()
+            if (n != game.length) {
+                repeat();
+            } else if (n == game.length) {
+                user = [];
+                userCount = 0;
+                n = 0;
+                clear();
+                Machine();
+            }
+        }, 750)
 }
